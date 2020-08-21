499aa7abaa4a706c31175a48c0e5e20a1bd4e3d0
Steve Anderson <prototype909@msn.com>
2020-08-21 18:51:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ let allWagesFor = function () {
 
     let payable = eligibleDates.reduce(function (memo, d) {
         return memo + wagesEarnedOnDate.call(this, d);
-    }.bind(this), 0) 
+    }.bind(this), 0)
 
     return payable;
 };
